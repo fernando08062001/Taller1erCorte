757f690a0888c5181f31c3bbf7e2d384cde9136e
Fernando Fiorillo <fernando_fiorilllo@hotmail.com>
2022-03-02 20:15:10 -0500

    punto 2

---
+++ punto 3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 20:06:28 2022
+
+@author: ffiorill
+"""
+
+vend = int(input("Sueldo base del vendedor al mes: "))
+ventas = int(input("Total de ventas realizadas por el vendedor: "))
+
+porcventas = ventas * 0.15
+
+pagomes = vend + porcventas
+
+print(f"EL total ganado por el vendedor en el mes de febrero seria: {pagomes}")
